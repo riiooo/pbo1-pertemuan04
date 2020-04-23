af25f4fecfe795fca58205c3d97fe4f4ef8a8142
riiooo <riokeparat46@gmail.com>
2020-04-23 12:27:29 +0800

    Array1

---
+++ src/Array/Array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+    }
+}
