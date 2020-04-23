a61a38c8d087e93f5393cae0f57f15e0f3f967de
riiooo <riokeparat46@gmail.com>
2020-04-23 12:32:22 +0800

    Array2

---
+++ src/Array/Arrayy.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Arrayy {
+    public static void main(String[] args) {
+        String matakuliah[];
+        matakuliah = new String[10];
+
+        matakuliah[0] = "Pemrograman Berbasis Objek 1";
+        matakuliah[1] = " Visual 2";
+        matakuliah[2] = " Rekayasa Perangkat Lunak";
+        matakuliah[3] = " Organisasi Komputer";
+        matakuliah[4] = " Pemrograman Web 2";
+    }
+}
